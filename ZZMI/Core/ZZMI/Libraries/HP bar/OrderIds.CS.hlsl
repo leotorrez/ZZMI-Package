32b6455572d26d359af46460fdb2c44147f0df3e
Satan1c <stg.nytos@gmail.com>
2026-08-03 00:32:19 +0300

    HP bar 3.1 update

--- ZZMI/Core/ZZMI/Libraries/HP bar/OrderIds.CS.hlsl
+++ ZZMI/Core/ZZMI/Libraries/HP bar/OrderIds.CS.hlsl
@@ -1,110 +1,114 @@
 struct accumulator
 {
-	float counter;
+	uint counter;
 	float summ;
 };
 struct stored
 {
-	float3 hp;
-	float3 hp_pos;
+	uint3 hp;
+	int3  hp_pos;
 	accumulator hp_acc;
 
-	float3 id;
-	float3 id_pos;
+	uint3 id;
+	int3  id_pos;
 	accumulator id_acc;
 };
 RWStructuredBuffer<stored> u0 : register(u0);
 Buffer<float> vb0 : register(t0);
 
-
 #define cmp -
 Texture1D<float4> IniParams : register(t120);
 
-
-#define first_vertex IniParams[1].x
-#define total IniParams[1].y
-#define value IniParams[1].z
-
-void swap(inout int a, inout int b)
-{
-	if (a == b) return;
-	a ^= b;
-	b ^= a;
-	a ^= b;
-}
+#define first_vertex	IniParams[1].x
+#define total_raw		IniParams[1].y
+#define value_raw		IniParams[1].z
+#define stride			IniParams[1].w
 
 [numthreads(1, 1, 1)]
 void main()
 {
 	stored data = u0[0];
-	accumulator acc = data.id_acc;
-	if (acc.counter < 3)
+
+	uint counter = data.id_acc.counter;
+	if (counter >= 3)
+		return;
+
+	uint total = (uint)total_raw;
+
+	uint index = first_vertex * stride / 4;
+	int positionX = vb0[index];
+	uint value = (uint)value_raw;
+
+	uint3 id = data.id;
+	int3  pos = data.id_pos;
+
+	if (counter == 0)
 	{
-		uint index = first_vertex;
-		if (vb0[5] == -1) {
-			index = index * 76 / 4;
-		} else if (vb0[1] == 0) {
-			index = index * 20 / 4;
-		}
-		float positionX = vb0[index];
+		id.x = value;
+		pos.x = positionX;
+	}
+	else if (counter == 1)
+	{
+		id.y = value;
+		pos.y = positionX;
 
-		uint3 id = data.id;
-		int3 pos = data.id_pos;
-
-		switch (acc.counter)
+		if (positionX < pos.x)
 		{
-			case 0:
-				id.x = value;
-				pos.x = positionX;
-				break;
-			case 1:
-				id.y = value;
-				pos.y = positionX;
-				if (positionX < pos.x)
-				{
-					swap(id.x, id.y);
-					swap(pos.x, pos.y);
-				}
-				break;
-			case 2:
-				id.z = value;
-				pos.z = positionX;
-				if (positionX < pos.y)
-				{
-					swap(id.y, id.z);
-					swap(pos.y, pos.z);
+			uint tid = id.x;
+			id.x = id.y;
+			id.y = tid;
 
-					if (positionX < pos.x)
-					{
-						swap(id.x, id.y);
-						swap(pos.x, pos.y);
-					}
-				}
-				break;
+			int  tpos = pos.x;
+			pos.x = pos.y;
+			pos.y = tpos;
 		}
+	}
+	else
+	{
+		id.z = value;
+		pos.z = positionX;
 
-		data.id = id;
-		data.id_pos = pos;
-
-		acc.counter++;
-
-		if (acc.counter == total)
+		if (positionX < pos.y)
 		{
-			switch (total)
+			uint tid = id.y;
+			id.y = id.z;
+			id.z = tid;
+
+			int  tpos = pos.y;
+			pos.y = pos.z;
+			pos.z = tpos;
+
+			if (positionX < pos.x)
 			{
-				case 1:
-					acc.summ = (id.x * 10201) + 0;
-					break;
-				case 2:
-					acc.summ = (id.x * 10201) + (id.y * 101) + 0;
-					break;
-				case 3:
-					acc.summ = (id.x * 10201) + (id.y * 101) + id.z;
-					break;
+				tid = id.x;
+				id.x = id.y;
+				id.y = tid;
+
+				tpos = pos.x;
+				pos.x = pos.y;
+				pos.y = tpos;
 			}
 		}
-
-		data.id_acc = acc;
-		u0[0] = data;
 	}
+
+	data.id = id;
+	data.id_pos = pos;
+
+	counter++;
+	data.id_acc.counter = counter;
+
+	if (counter == total)
+	{
+		uint packed = id.x << 16;
+
+		if (total > 1)
+			packed |= id.y << 8;
+
+		if (total > 2)
+			packed |= id.z;
+
+		data.id_acc.summ = packed;
+	}
+
+	u0[0] = data;
 }
--- ZZMI/Core/ZZMI/Libraries/HP bar/SummHp.CS.hlsl
+++ ZZMI/Core/ZZMI/Libraries/HP bar/SummHp.CS.hlsl
@@ -1,16 +1,16 @@
 struct accumulator
 {
-	float counter;
+	uint counter;
 	float summ;
 };
 struct stored
 {
-	float3 hp;
-	float3 hp_pos;
+	uint3 hp;
+	int3 hp_pos;
 	accumulator hp_acc;
 
-	float3 id;
-	float3 id_pos;
+	uint3 id;
+	int3 id_pos;
 	accumulator id_acc;
 };
 RWStructuredBuffer<stored> u0 : register(u0);
@@ -18,100 +18,111 @@ RWStructuredBuffer<stored> u0 : register(u0);
 Buffer<float> vb0 : register(t0);
 cbuffer cb1 : register(b1)
 {
-	float4 cb1[24];
+	float4 cb1[29];
 }
 
 #define cmp -
 Texture1D<float4> IniParams : register(t120);
 
 
-#define first_vertex IniParams[1].x
-#define total IniParams[1].y
+#define first_vertex	IniParams[1].x
+#define total_raw		IniParams[1].y
+#define stride			IniParams[1].z
 
-void swap(inout uint a, inout uint b)
-{
-	if (a == b) return;
-	a ^= b;
-	b ^= a;
-	a ^= b;
-}
 
 [numthreads(1, 1, 1)]
 void main()
 {
+	float z = cb1[12].z;
+	if (z <= 0.39 || z >= 0.61)
+		return;
+
 	stored data = u0[0];
-	accumulator acc = data.hp_acc;
-	float cb_12_x = cb1[12].x;
 
-	if (acc.counter < 3 && cb_12_x > 0.39 && cb_12_x < 0.61)
+	uint counter = data.hp_acc.counter;
+	if (counter >= 3)
+		return;
+
+	uint total = (uint)total_raw;
+
+	uint index = first_vertex * stride / 4;
+	int positionX = vb0[index];
+	uint value = (cb1[11].z + cb1[11].w) * 100.0 + 0.5;
+
+	uint3 hp = data.hp;
+	int3 pos = data.hp_pos;
+
+	if (counter == 0)
 	{
-		uint index = first_vertex;
-		if (vb0[5] == -1) {
-			index = index * 76 / 4;
-		} else if (vb0[1] == 0) {
-			index = index * 20 / 4;
-		}
-		int positionX = vb0[index];
-		float2 cb_11 = cb1[11].yz;
-		uint value = floor((cb_11.x + cb_11.y) * 100);
+		hp.x = value;
+		pos.x = positionX;
+	}
+	else if (counter == 1)
+	{
+		hp.y = value;
+		pos.y = positionX;
 
-		uint3 hp = data.hp;
-		int3 pos = data.hp_pos;
-
-		switch (acc.counter)
+		if (positionX < pos.x)
 		{
-			case 0:
-				hp.x = value;
-				pos.x = positionX;
-				break;
-			case 1:
-				hp.y = value;
-				pos.y = positionX;
-				if (positionX < pos.x)
-				{
-					swap(hp.x, hp.y);
-					swap(pos.x, pos.y);
-				}
-				break;
-			case 2:
-				hp.z = value;
-				pos.z = positionX;
-				if (positionX < pos.y)
-				{
-					swap(hp.y, hp.z);
-					swap(pos.y, pos.z);
+			uint th = hp.x;
+			hp.x = hp.y;
+			hp.y = th;
 
-					if (positionX < pos.x)
-					{
-						swap(hp.x, hp.y);
-						swap(pos.x, pos.y);
-					}
-				}
-				break;
+			int tp = pos.x;
+			pos.x = pos.y;
+			pos.y = tp;
 		}
+	}
+	else
+	{
+		hp.z = value;
+		pos.z = positionX;
 
-		data.hp = hp;
-		data.hp_pos = pos;
-
-		acc.counter++;
-
-		if (acc.counter == total)
+		if (positionX < pos.y)
 		{
-			switch (total)
+			uint th = hp.y;
+			hp.y = hp.z;
+			hp.z = th;
+
+			int tp = pos.y;
+			pos.y = pos.z;
+			pos.z = tp;
+
+			if (positionX < pos.x)
 			{
-				case 1:
-					acc.summ = (hp.x * 10201) + 10200;
-					break;
-				case 2:
-					acc.summ = (hp.x * 10201) + (hp.y * 101) + 100;
-					break;
-				case 3:
-					acc.summ = (hp.x * 10201) + (hp.y * 101) + hp.z;
-					break;
+				th = hp.x;
+				hp.x = hp.y;
+				hp.y = th;
+
+				tp = pos.x;
+				pos.x = pos.y;
+				pos.y = tp;
 			}
 		}
-
-		data.hp_acc = acc;
-		u0[0] = data;
 	}
+
+	data.hp = hp;
+	data.hp_pos = pos;
+
+	counter++;
+	data.hp_acc.counter = counter;
+
+	if (counter == total)
+	{
+		uint packed = hp.x << 14;
+
+		if (total > 1)
+			packed |= hp.y << 7;
+		else
+			packed |= 12900u;
+
+		if (total > 2)
+			packed |= hp.z;
+		else if (total == 2)
+			packed |= 100u;
+
+		data.hp_acc.summ = packed;
+	}
+
+	u0[0] = data;
 }
